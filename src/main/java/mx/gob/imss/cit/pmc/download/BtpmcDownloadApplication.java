fd874763ff04b24e73790251248841d984cf6821
Colaborador3 <151958259+ADMR02@users.noreply.github.com>
2023-11-30 13:21:10 -0600

    Versión QA
    Se agrega código con versionado de QA OCP4.

---
+++ src/main/java/mx/gob/imss/cit/pmc/download/BtpmcDownloadApplication.java
@@ -0,0 +1,13 @@
+package mx.gob.imss.cit.pmc.download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtpmcDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtpmcDownloadApplication.class, args);
+	}
+
+}
